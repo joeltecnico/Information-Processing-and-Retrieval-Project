ff88ec6bbb6acb6ba8dc8bb3a559218b94578a21
RitaRamos <rita.mparada.ramos@gmail.com>
2017-10-23 12:23:16 +0100

    ja faz a comparação dos 2 exercicios. Falta o precision, recall,etc

--- exercise-2.py
+++ exercise-2.py
@@ -10,7 +10,7 @@ import numpy as np
 import operator
 import os
 import codecs
-
+import proj1
 
 def counts_and_tfs(file_content, vec):
     counts_of_terms=vec.fit_transform(file_content).toarray()  #numpy array com as respectivas contages dos termos (linha=doc,col=termo, value=contagem)
@@ -29,50 +29,62 @@ def doc_ToVectorSpace(content, number_of_docs):
     idfs=np.log10(number_of_docs/(counts_of_terms_doc != 0).sum(0))  # idfs= log10(len dos docs/ contagem dos docs q tem esse termo)
     return tfs_doc*idfs, vec.vocabulary_
 
-def cosine_similarity(sentences_vectors,doc_vector):
-    cosSim={}
-    i=0
-    for sentence_vector in sentences_vectors:
-        cosSim[i]= np.dot(sentence_vector,doc_vector)/(np.sqrt(  np.sum(sentence_vector*sentence_vector) )* np.sqrt(np.sum(doc_vector*doc_vector) ))
-        i+=1
-    return cosSim
 
-
-def convert_and_docs_ToVectorSpace(path):
+def ex1_sentences_and_docs_ToVectorSpace(path):
     docs_sentences={}
     i=0
     docs_content=[]
+    ex1_docs_sentences_vectors={}
+    ex1_docs_vectors={}
+    
     for root, dirs, files in os.walk(path):
         for f in files:
             file_content = open(os.path.join(root, f), "rb").read().decode('iso-8859-1')
-            docs_sentences[i] = nltk.sent_tokenize(file_content) #o doc dividido em frases
-            docs_content.append(file_content) #vão sendo guardado os documentos todos
+            sentences=nltk.sent_tokenize(file_content) #o doc dividido em frases
+            
+            ex1_docs_sentences_vectors[i], isfs=proj1.sentences_ToVectorSpace(sentences)   #converter frases para vectores, usando ex 1
+            ex1_docs_vectors[i]=proj1.doc_ToVectorSpace(file_content, isfs)                #converter doc para vector, usando ex 1 (argument2: inverse sentence frequency)
+            
+            docs_sentences[i] = sentences     #vão sendo guardadas as frases para depois calcular para ex2
+            docs_content.append(file_content) #vão sendo guardado os documentos todos para depois calcular-se para ex2
             i+=1
-    
-    number_of_docs=len(docs_content)
-    docs_vectors, vocabulary=doc_ToVectorSpace(docs_content, number_of_docs)  #documentos em vector
-    
-    docs_sentences_vectors={}                                
+    return  ex1_docs_sentences_vectors, ex1_docs_vectors, docs_sentences,docs_content
+
+
+def ex2_sentences_and_docs_ToVectorSpace(docs_content,docs_sentences,number_of_docs ):
+    ex2_docs_vectors, vocabulary=doc_ToVectorSpace(docs_content, number_of_docs)  #converter docs em vector usando o ex2
+    ex2_docs_sentences_vectors={}                                
     for i in range(number_of_docs):
-        docs_sentences_vectors[i]=sentences_ToVectorSpace(docs_sentences[i], vocabulary)  #as frases passam a estar representadas em vectores
+        ex2_docs_sentences_vectors[i]=sentences_ToVectorSpace(docs_sentences[i], vocabulary)  #converter frases em vector usando o ex2 
+    return ex2_docs_sentences_vectors, ex2_docs_vectors
     
-    return docs_sentences_vectors, docs_vectors, docs_sentences
+
+def calculate_cosine_for_the_2_exs(ex1_vector_of_docsSentences,  ex1_vectors_of_docs, ex2_vector_of_docsSentences, ex2_vectors_of_docs, number_of_docs  ):    
+    ex1_cosSim_of_docs={}
+    ex2_cosSim_of_docs={}
+    for i in range(number_of_docs):
+        ex1_cosSim_of_docs[i]=proj1.cosine_similarity(ex1_vector_of_docsSentences[i] , ex1_vectors_of_docs[i][0])
+        ex2_cosSim_of_docs[i]=proj1.cosine_similarity(ex2_vector_of_docsSentences[i] , ex2_vectors_of_docs[i])
+        show_summary_for_the_2_exs(ex1_cosSim_of_docs[i],ex2_cosSim_of_docs[i], i)
 
     
+def show_summary_for_the_2_exs(ex1_cosSim,ex2_cosSim, id_doc):
+    doc_sentences=docs_sentences[id_doc]
+    print("\n For Doc1: ", id_doc)
+    print("\n Ex1 summary: ", proj1.show_summary(ex1_cosSim, doc_sentences))
+    print("\n Ex2 summary: ", proj1.show_summary(ex2_cosSim, doc_sentences))
+    #Calcular aqui o Precision e o recall
     
-def showResultsToUser(vector_of_docsSentences, vectors_of_docs):
-    docs_size=len(vectors_of_docs)
-    docs_cosSim={}
-    for i in range(docs_size):
-        docs_cosSim[i]=cosine_similarity(vector_of_docsSentences[i] , vectors_of_docs[i])
-        doci_sorted_scores = sorted(docs_cosSim[i].items(), key=operator.itemgetter(1),reverse=True) #Ponto 4 done
-        summary=sorted(doci_sorted_scores[0:3], key=operator.itemgetter(0)) 
-        print("\n Result to the user of doc ",i,": ",[docs_sentences[i][line] for line,sim in summary])  #Ponto 5 done, end!
-     
-vector_of_docsSentences, vectors_of_docs, docs_sentences=convert_and_docs_ToVectorSpace('Textos-fonte-com-titulo')
-#cosinesOfAllDos(vector_of_docsSentences, vectors_of_docs)
-showResultsToUser(vector_of_docsSentences, vectors_of_docs)
 
+
+#Results:
+ex1_vector_of_docsSentences, ex1_vectors_of_docs, docs_sentences, docs_content =ex1_sentences_and_docs_ToVectorSpace('Textos-fonte-com-titulo')
+number_of_docs=len(docs_sentences)
+ex2_vector_of_docsSentences, ex2_vectors_of_docs=ex2_sentences_and_docs_ToVectorSpace(docs_content, docs_sentences,number_of_docs )
+calculate_cosine_for_the_2_exs(ex1_vector_of_docsSentences, ex1_vectors_of_docs,ex2_vector_of_docsSentences, ex2_vectors_of_docs,number_of_docs)
+
+#Comentários:
+    # o output do ficheiro 1 (proj1.py) está a ser chamado, quando devia estar a correr apenas as funcoes que chamei)
     
     
     

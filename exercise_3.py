ae6598b4cd9310f3680be011539da342de01e311
RitaRamos <rita.mparada.ramos@gmail.com>
2017-10-28 17:40:22 +0100

    fiz refactor do exer_3, tal como ta no exer 2.

--- exercise_3.py
+++ exercise_3.py
@@ -7,14 +7,20 @@
 import nltk
 from sklearn.feature_extraction.text import CountVectorizer
 import numpy as np
-import operator
 import os
-from os import listdir
-from os.path import isfile, join
-import codecs
 import exercise_1
+import exercise_2
 import re
-import math
+
+AP_sum = 0
+precision_sum = 0
+recall_sum = 0
+
+def words_separation(sentences):
+    words=[]
+    for t in sentences:
+        words.append(re.findall(r'\w+',t.strip().lower()))
+    return words
 
 def tag_string(s) :
     sentence = ''
@@ -23,7 +29,7 @@ def tag_string(s) :
         sentence = ' '.join([sentence, b])
     return sentence
 
-def counts_and_tfs_biGrams(file_content, vec):
+def counts_and_tfs(file_content, vec):
     #sentences_found = []
     #separation = words_separation(file_content)
     #print(separation)
@@ -49,132 +55,60 @@ def counts_and_tfs_biGrams(file_content, vec):
     tfs=counts_of_terms/np.max(counts_of_terms, axis=1)[:, None]  #tf= freq/max termo
     return counts_of_terms,tfs
 
-def words_separation(sentences):
-    words=[]
-    for t in sentences:
-        words.append(re.findall(r'\w+',t.strip().lower()))
-    return words
- 
+
 def sentences_ToVectorSpace(content):
-    vec = CountVectorizer(ngram_range=(1, 2),token_pattern=r'\b\w+\b')  #é dado o vocabulario dos documentos
-    counts_of_terms_sent, tfs_sent=counts_and_tfs_biGrams(content, vec) #as contagens e os tfs para as frases
+    vec = CountVectorizer(ngram_range=(1, 2),token_pattern=r'\b\w+\b')  #vocabulario das frases do documento com unigrama e brigrama
+    counts_of_terms_sent, tfs_sent=counts_and_tfs(content, vec) #as contagens e os tfs para as frases
     isfs=np.log10(len(counts_of_terms_sent)/(counts_of_terms_sent != 0).sum(0))  # inverve sentence frequency= log10(len dos docs/ contagem dos docs q tem esse termo)
     return tfs_sent*isfs, isfs, vec.vocabulary_
 
 
 def doc_ToVectorSpace(content, isfs,docs_vocabulary ):
     vec = CountVectorizer(vocabulary=docs_vocabulary)
-    counts_of_terms_doc, tfs_doc=counts_and_tfs_biGrams([content], vec)  # as contagens e tfs para o documento
+    counts_of_terms_doc, tfs_doc=counts_and_tfs([content], vec)  # as contagens e tfs para o documento
     return tfs_doc*isfs
 
 def sentences_and_docs_ToVectorSpace(path):
     docs_sentences={}
     i=0
-    docs_content=[]
     docs_sentences_vectors={}
     docs_vectors={}
     
     for root, dirs, files in os.walk(path):
         for f in files:
-            file_content = open(os.path.join(root, f), 'rb').read().decode('iso-8859-1')
-            file_with_splitLines = file_content.splitlines()
-            
-            sentences=[]
-            for line in file_with_splitLines:
-                sentences+=nltk.sent_tokenize(line)
-                
+            file_content,sentences= exercise_1.getFile_and_separete_into_sentences(os.path.join(root, f))
+           
             docs_sentences_vectors[i], isfs, vocabulary=sentences_ToVectorSpace(sentences)   #converter frases para vectores, usando ex 1
             docs_vectors[i]=doc_ToVectorSpace(file_content, isfs, vocabulary)                #converter doc para vector, usando ex 1 (argument2: inverse sentence frequency)
-            
-          
+        
             docs_sentences[i] = sentences     #vão sendo guardadas as frases para depois calcular para ex2
-            docs_content.append(file_content) #vão sendo guardado os documentos todos para depois calcular-se para ex2
             i+=1
-            
-            
-    return docs_sentences_vectors,docs_vectors,  docs_sentences,docs_content
-
+                  
+    return docs_sentences_vectors,docs_vectors,  docs_sentences
 
 
 def calculate_cosine_for_the_ex(vector_of_docsSentences,  vectors_of_docs, number_of_docs):    
-    AP_sum = 0
-    precision_sum = 0
-    recall_sum = 0
     cosSim_of_docs={}
     for i in range(number_of_docs):
         cosSim_of_docs[i]=exercise_1.cosine_similarity(vector_of_docsSentences[i] , vectors_of_docs[i][0])
+        show_summary(cosSim_of_docs[i], i)
         
-        AP, precision, recall = show_summary(cosSim_of_docs[i], i, AP_sum)
-        AP_sum += AP
-        precision_sum += precision
-        recall_sum += recall
-    MAP = AP_sum / number_of_docs
-    print('MAP ex1 ', MAP)
-    print('Recall ex1 ', recall_sum / number_of_docs)
-    print('Precision ex1 ', precision_sum / number_of_docs)
 
-    
-def show_summary(ex1_cosSim, id_doc, AP_sum):
+def show_summary(cosSim, id_doc):
     doc_sentences=docs_sentences[id_doc]
-    ex1_summary_to_user, ex1_summary=exercise_1.show_summary(ex1_cosSim, doc_sentences)
-    print('\n For Doc1: ', id_doc)
-    print('\n Ex3 summary: ', ex1_summary_to_user)
-    
-   
-    mypath = 'TeMario/Sumários/Extratos ideais automáticos'
-    onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))] #Isto ficaria melhor se mantivessemos uma lista com os nomes dos ficheiros...
-    ##/HORRIBLE DANGER
-    
-    
-    summary_content = open(mypath + '/' + onlyfiles[id_doc], 'rb').read().decode('iso-8859-1')
-    summary_splitted = summary_content.splitlines()
-    
-    summary_sentences=[]
-    for line in summary_splitted:
-        summary_sentences+=nltk.sent_tokenize(line)
-        
-    RuA1 = sum(1 for x in ex1_summary_to_user if x in summary_content)
-    precision1 = RuA1 / len(ex1_summary_to_user)
-    recall1 = RuA1 / len(summary_sentences)
-    f11 = 2 * np.float64(precision1 * recall1) / (precision1 + recall1) #Sometimes F1 can end up dividing by zero, this will give either inf or NaN as a result
-    
-   
-    print('\nPrecision on Ex3', precision1)
-    print('Recall on Ex3', recall1)
-    print('F1 on Ex3', f11)
-   
-    
-    #Calculate PA
-    # Ex1
-    precision_recall_curve_ex1 = []
-    correct_until_now = 0
-    for i in range(len(ex1_summary_to_user)) :
-        if ex1_summary_to_user[i] in summary_content :
-            correct_until_now +=1
-            precision = correct_until_now / (i+1)
-            recall = correct_until_now / len(summary_sentences)
-            precision_recall_curve_ex1.append((recall, precision))
-    print('Iguais ', correct_until_now)
-    print(precision_recall_curve_ex1)
-    
-    total=0
-    last_index=1
-    for (R,P) in precision_recall_curve_ex1:
-        part,truncated_index = math.modf(R * 10)
-        truncated_index=int(truncated_index)
-        for R in range(last_index, truncated_index+1):
-            total += (truncated_index * 0.1) *P
-            
-        last_index=truncated_index+1
-    
-    AP = 0
-    if correct_until_now > 0:
-        AP = total / correct_until_now
-    return AP, precision1, recall1
-    
+    summary_to_user, summary=exercise_1.show_summary(cosSim, doc_sentences, 5)
+    evaluate_summaries(summary_to_user,id_doc)
 
 
-#Results:
-vector_of_docsSentences,vectors_of_docs, docs_sentences, docs_content =sentences_and_docs_ToVectorSpace('TeMario/Textos-fonte/Textos-fonte com título')
-number_of_docs=len(docs_sentences)
-calculate_cosine_for_the_ex(vector_of_docsSentences, vectors_of_docs,number_of_docs)
+def evaluate_summaries( summary_to_user, id_doc):
+    ideal_summary,ideal_summary_sentences =exercise_1.getFile_and_separete_into_sentences(ideal_summaries_filesPath[id_doc])  
+    global AP_sum, precision_sum,recall_sum
+    AP_sum, precision_sum,recall_sum=  exercise_2.calculate_precision_recall_ap(summary_to_user, ideal_summary, ideal_summary_sentences,AP_sum, precision_sum,recall_sum)
+
+    
+if __name__ == "__main__":
+    ideal_summaries_filesPath=exercise_2.get_ideal_summaries_files('TeMario/Sumários/Extratos ideais automáticos')
+    vector_of_docsSentences,vectors_of_docs, docs_sentences =sentences_and_docs_ToVectorSpace('TeMario/Textos-fonte/Textos-fonte com título')
+    number_of_docs=len(docs_sentences)
+    calculate_cosine_for_the_ex(vector_of_docsSentences, vectors_of_docs,number_of_docs)
+    exercise_2.print_results("exercise 3.1", (precision_sum / number_of_docs),(recall_sum / number_of_docs),(AP_sum/number_of_docs))

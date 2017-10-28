597725a61842ce38dc645b27085ccca2f8d251ff
RitaRamos <rita.mparada.ramos@gmail.com>
2017-10-28 10:38:59 +0100

    Pus o getFile e Separate into sentences igual aos outros docs. Pus comentários para o prof quando for a ler for mais facil; e uma vz que o vec das frases é o mesmo que o doc, basta calcular-se o vec nas frases e dar como argumento ao doc.

--- exercise_1.py
+++ exercise_1.py
@@ -1,5 +1,5 @@
 # Joel Almeida		81609
-# Matilde Gonçalves	82091
+# Matilde Gonรงalves	82091
 # Rita Ramos		86274
 
 
@@ -8,22 +8,27 @@ from sklearn.feature_extraction.text import CountVectorizer
 import numpy as np
 import operator
 
-def read_file(file):
-    return open(file).read()
+def getFile_and_separete_into_sentences(f): #quer para ingles, quer para português (semelhantes)
+    file_content = open(f, 'rb').read().decode('iso-8859-1')
+    file_content_splitted = file_content.splitlines()
+    sentences=[]
+    for line in file_content_splitted:
+        sentences+=nltk.sent_tokenize(line)
+    return file_content,sentences 
     
-def counts_and_tfs(file_content):
-    vec = CountVectorizer()
-    counts_of_terms=vec.fit_transform(file_content).toarray()  #numpy array com as respectivas contages dos termos (linha=doc,col=termo, value=contagem)
+def counts_and_tfs(file_content, vec):
+    counts_of_terms=vec.fit_transform(file_content).toarray()  #numpy array com as respectivas contages dos termos (linhas=doc or sent,cols=termo, values=contagem)
     tfs=counts_of_terms/np.max(counts_of_terms, axis=1)[:, None]  #tf= freq/max termo
     return counts_of_terms,tfs
 
 def sentences_ToVectorSpace(content):
-    counts_of_terms_sent, tfs_sent=counts_and_tfs(content) #as contagens e os tfs para as frases
-    isfs=np.log10(len(counts_of_terms_sent)/(counts_of_terms_sent != 0).sum(0))  # inverve sentence frequency= log10(len dos docs/ contagem dos docs q tem esse termo)
-    return tfs_sent*isfs, isfs
+    vec = CountVectorizer()
+    counts_of_terms_sent, tfs_sent=counts_and_tfs(content, vec) #as contagens numpy(linhas=sent, cols=termos) e os tfs
+    isfs=np.log10(len(counts_of_terms_sent)/(counts_of_terms_sent != 0).sum(0))  # inverve sentence frequency= log10(nº frases do doc/ contagem das frases q tem esse termo)
+    return tfs_sent*isfs, isfs, vec
 
-def doc_ToVectorSpace(content, isfs):
-    counts_of_terms_doc, tfs_doc=counts_and_tfs([content])  # as contagens e tfs para o documento
+def doc_ToVectorSpace(content, isfs, vec):
+    counts_of_terms_doc, tfs_doc=counts_and_tfs([content], vec)  # as contagens numpy(linhas=doc, cols=termos) e os tfs
     return tfs_doc*isfs
 
 def cosine_similarity(sentences_vectors,doc_vector):
@@ -36,17 +41,17 @@ def cosine_similarity(sentences_vectors,doc_vector):
 
 
 def show_summary(scored_sentences, sentences, number_of_top_sentences):
-    scores_sentences_sorted = sorted(scored_sentences.items(), key=operator.itemgetter(1),reverse=True) 
-    summary=sorted(scores_sentences_sorted[0:number_of_top_sentences], key=operator.itemgetter(0))  #Ponto 4 done
-    return [sentences[line] for line,sim in summary], summary
+    scores_sentences_sorted = sorted(scored_sentences.items(), key=operator.itemgetter(1),reverse=True)  # ordenar os scores
+    summary=sorted(scores_sentences_sorted[0:number_of_top_sentences], key=operator.itemgetter(0))  #extrair as x frases mais relevantes summary= (id_sentence, score)  (Ponto 4 done)
+    return [sentences[line] for line,sim in summary], summary #imprimir as frases; imprimir summary= (id_sentence, score)
     
 
 if __name__ == "__main__":
-    file_content=read_file("script1.txt")
-    sentences = nltk.sent_tokenize(file_content) #o doc dividido em frases
     
-    sentences_vectors, isfs=sentences_ToVectorSpace(sentences)  #Ponto 1
-    doc_vector=doc_ToVectorSpace(file_content, isfs)    #Ponto 2
+    file_content, sentences=getFile_and_separete_into_sentences("script1.txt")            
+                                
+    sentences_vectors, isfs, vec=sentences_ToVectorSpace(sentences)  #Ponto 1
+    doc_vector=doc_ToVectorSpace(file_content, isfs, vec)    #Ponto 2
     print("The vectors of the sentences:\n", sentences_vectors,"\n\n The vector of the document:\n", doc_vector)    
     
     scored_sentences=cosine_similarity(sentences_vectors,doc_vector[0])  #Ponto 3 done

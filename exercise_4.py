bca6c840f9a298567a98998db97b74d45576b918
RitaRamos <rita.mparada.ramos@gmail.com>
2017-11-01 12:36:28 +0000

    Exercice 4 com o lambda correcto

--- exercise_4.py
+++ exercise_4.py
@@ -29,7 +29,7 @@ def cosine_similarity_asNumpy(sentences_vectors,doc_vector):
 
 def calculate_mmr_oficial(vector_of_docsSentences,vectors_of_docs, number_of_docs, docs_sentences):
     summary_of_docs=defaultdict(list)
-    y=0
+    y=1/5
     
     for summary_level in range(5):
         for i in range(number_of_docs):
@@ -85,46 +85,3 @@ if __name__ == "__main__":
     show_summary_ordered(summary_of_docs,number_of_docs )
     exercise_2.print_results("exercise 4- alternative MMR", (precision_sum_MMR / number_of_docs),(recall_sum_MMR  / number_of_docs),(AP_sum_MMR /number_of_docs))
     exercise_2.print_results("exercise 4 - alternative top 5", (precision_sum_Top5 / number_of_docs),(recall_sum_Top5 / number_of_docs),(AP_sum_Top5/number_of_docs))
-
-            
-    
-                
-            
-            
-    
-         
-            
-            
-    
-        
-        
-    
-
-    
-
-
-'''JOEL PART
-
-def calculate_mmr_for_the_2_exs(ex1_vector_of_docsSentences,  ex1_vectors_of_docs, ex2_vector_of_docsSentences, ex2_vectors_of_docs, number_of_docs):    
-    ex1_cosSim_of_docs={}
-    ex2_cosSim_of_docs={}
-    l = 0.2
-    print("For doc1")
-    selected = collections.OrderedDict()
-    for i in range(number_of_docs):
-        for sentence in ex1_vector_of_docsSentences[i] :
-            max_sim2 = 0
-            for key in selected :
-                max_sim2 = max(max_sim2, exercise_1.cosine_similarity(sentence, selected[key]))
-                
-            mmr = (1 - l) * exercise_1.cosine_similarity(np.array([sentence]), ex1_vector_of_docsSentences[i][0])[0] - l * max_sim2
-            print('MMR Doc ', i, ' ', mmr)
-        break
-
-
-if __name__ == "__main__":
-    vector_of_docsSentences, vectors_of_docs, docs_sentences, docs_content = exercise_2.ex1_sentences_and_docs_ToVectorSpace('TeMario/Textos-fonte/Textos-fonte com título')
-    number_of_docs=len(docs_sentences)
-    ex2_vector_of_docsSentences, ex2_vectors_of_docs=exercise_2.ex2_sentences_and_docs_ToVectorSpace(docs_content, docs_sentences,number_of_docs )
-    calculate_mmr_for_the_2_exs(ex1_vector_of_docsSentences, ex1_vectors_of_docs,ex2_vector_of_docsSentences, ex2_vectors_of_docs,number_of_docs)
-    '''

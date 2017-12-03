90c31e4cf89166d1a61451ab8a708165f5a33f69
RitaRamos <rita.mparada.ramos@gmail.com>
2017-12-03 21:52:23 +0000

    Remove stop words

--- part2/exercise_2.py
+++ part2/exercise_2.py
@@ -17,7 +17,41 @@ import os
 import exercise_1
 
 ex1_AP_sum = 0
+
 ex2_AP_sum = 0
+ex1_precision_sum=0
+ex2_precision_sum=0
+
+
+stop_words=nltk.corpus.stopwords.words('portuguese')
+
+'''
+def remove_stop_words(sentences): #para cada frase, vamos retirar as stop words
+    words=[]
+    for sentence in sentences:
+        words_of_sentence=[word for word in re.findall(r'\w+', sentence.lower()) if word
+            not in arrayStopWords]
+        if len(words_of_sentence) >1: #Apos remover stopWrds, verificar se a frase não ficou vazia
+            words.append(words_of_sentence)
+    return words
+
+'''
+
+def getFile_and_separete_into_sentences(f): 
+    file_content = open(f, 'rb').read().decode('iso-8859-1')
+    file_content_splitted = file_content.splitlines()
+    sentences_without_stop_words=[]
+    for line in file_content_splitted:
+        for sentence in nltk.sent_tokenize(line):
+            words_without_stop_words=[word for word in re.findall(r'\w+', sentence.lower()) if word
+            not in stop_words]
+            if len(words_without_stop_words)>0: #check:not only pontuaction
+                sentence=' '.join(words_without_stop_words)
+                sentences_without_stop_words.append(sentence)
+    file_without_stop_words=' '.join(sentences_without_stop_words)
+    
+    #print ("\n File content", file_content)
+    return file_without_stop_words,sentences_without_stop_words
 
 
 def get_ideal_summaries_files(path):
@@ -34,26 +68,32 @@ def read_docs(path):
     
     for root, dirs, files in os.walk(path):
         for f in files:
-            file_content,sentences=exercise_1.getFile_and_separete_into_sentences(os.path.join(root, f))
+            file_content, sentences=getFile_and_separete_into_sentences(os.path.join(root, f))
     
             ex1_sents_represented=exercise_1.sentences_ToVectorSpace(sentences)  
             ex1_graph=exercise_1.get_graph(ex1_sents_represented, 0.2)     
             ex1_PR = exercise_1.calculate_page_rank(ex1_graph, 0.15, 50)
             ex1_summary, ex1_summary_to_user=exercise_1.show_summary(ex1_PR,sentences,5)
             
-       
+            
             #ex2_summary, ex2_summary_to_user=PR_priorsPosition_weightsTFIDFS(sentences)
             #ex2_summary, ex2_summary_to_user=PR_priorsTFIDFS_weightsTFIDFS(sentences)
-            ex2_summary, ex2_summary_to_user=PR_priorsLenSents_weightsTFIDFS(sentences)
-            
+            #ex2_summary, ex2_summary_to_user=PR_priorsLenSents_weightsTFIDFS(sentences)
+            ex2_summary, ex2_summary_to_user=PR_priorsPositionAndLenSents_weightsTFIDFS(sentences)
+            #ex2_summary, ex2_summary_to_user=PR_priorsPosition_weightsBM25(sentences)
+
+
             print("\nDoc ",i, ": \n\nEx1- Summary to user:", ex1_summary_to_user, 
               ": \n\nEx2- Summary to user:", ex2_summary_to_user)
             
-            ideal_summary,ideal_summary_sentences =exercise_1.getFile_and_separete_into_sentences( ideal_summaries_filesPath[i])  
-            global ex1_AP_sum,  ex2_AP_sum
+            ideal_summary,ideal_summary_sentences =getFile_and_separete_into_sentences( ideal_summaries_filesPath[i])  
+            
+            print("ideal_summary", ideal_summary)
+            
+            global ex1_AP_sum,  ex2_AP_sum, ex1_precision_sum,ex2_precision_sum
             if(len(ex1_summary))>0:
-                ex1_AP_sum=  calculate_precision_recall_ap(ex1_summary,ideal_summary, ideal_summary_sentences,ex1_AP_sum)
-            ex2_AP_sum=  calculate_precision_recall_ap(ex2_summary,ideal_summary, ideal_summary_sentences,ex2_AP_sum)
+                ex1_AP_sum,ex1_precision_sum =  calculate_precision_recall_ap(ex1_summary,ideal_summary, ideal_summary_sentences,ex1_AP_sum, ex1_precision_sum)
+            ex2_AP_sum, ex2_precision_sum=  calculate_precision_recall_ap(ex2_summary,ideal_summary, ideal_summary_sentences,ex2_AP_sum, ex2_precision_sum)
             
             #break
             i+=1
@@ -64,7 +104,7 @@ def read_docs(path):
 def PR_priorsPosition_weightsTFIDFS(sentences):
     sentences_vectors, isfs, counts_of_terms_sent= sentences_ToVectorSpace(sentences)
     graph,indexes, indexes_sents_not_linked=get_graph(sentences_vectors)
-    priors=get_prior(len(graph))
+    priors=get_prior_Position(len(sentences_vectors),indexes_sents_not_linked)
     PR=calculate_improved_prank(graph, 0.15,50,  priors, indexes)
     print("\n SOMA", sum(list(PR.values())))
     summary, summary_to_user=exercise_1.show_summary(PR,sentences,5)
@@ -88,7 +128,17 @@ def PR_priorsLenSents_weightsTFIDFS(sentences):
     print("\n SOMA", sum(list(PR.values())))
     summary, summary_to_user=exercise_1.show_summary(PR,sentences,5)
     return summary, summary_to_user
-    
+
+def PR_priorsPositionAndLenSents_weightsTFIDFS(sentences):
+    sentences_vectors, isfs, counts_of_terms_sent= sentences_ToVectorSpace(sentences)
+    graph,indexes, indexes_sents_not_linked=get_graph(sentences_vectors)
+    priors=get_prior_PositionAndLenSents(counts_of_terms_sent, indexes_sents_not_linked)
+    PR=calculate_improved_prank(graph, 0.15,50,  priors, indexes)
+    print("\n SOMA", sum(list(PR.values())))
+    summary, summary_to_user=exercise_1.show_summary(PR,sentences,5)
+    return summary, summary_to_user
+
+
 
 def get_prior_TFIDF(doc_vector, sentences_vectors,indexes_not_linked ):
     priors_cos=cosine_similarity(doc_vector[0], sentences_vectors)
@@ -96,23 +146,59 @@ def get_prior_TFIDF(doc_vector, sentences_vectors,indexes_not_linked ):
     priors_cos=np.delete(priors_cos, indexes_not_linked,1) #delete rows with just zeros
     return priors_cos/np.sum(priors_cos)
 
-def get_prior(n_docs):
-    weights_pos=np.arange(n_docs, 0, -1)
-    return weights_pos/np.sum(weights_pos)
-
-
-def cosine_similarity(main_sentence,sentences_vectors ):
-    cosSim=[]
-    for sentence_vector in sentences_vectors:
-        cosSim.append( np.dot(sentence_vector,main_sentence)/(np.sqrt(
-            np.sum(sentence_vector*sentence_vector) )* np.sqrt(np.sum(main_sentence*main_sentence) )))
-    return np.array(cosSim) 
+def get_prior_Position(n_docs,indexes_not_linked):
+    priors_pos=np.arange(n_docs, 0, -1)
+    priors_pos=np.expand_dims(priors_pos, axis=0)
+    priors_pos=np.delete(priors_pos, indexes_not_linked,1) 
+    return priors_pos/np.sum(priors_pos)
 
 def get_prior_lenSents(counts_of_terms_sent,indexes_not_linked ): # dar + importancia as frases q tem + termos
     priors_len=(counts_of_terms_sent != 0).sum(1)    
     priors_len=np.expand_dims(priors_len, axis=0)
     priors_len=np.delete(priors_len, indexes_not_linked,1) 
     return priors_len/np.sum(priors_len)
+
+def get_prior_PositionAndLenSents(counts_of_terms_sent,indexes_not_linked ): # dar + importancia as frases q tem + termos
+    priors_len=np.arange(len(counts_of_terms_sent), 0, -1)* (counts_of_terms_sent != 0).sum(1) 
+    print("priors_len", priors_len)
+    priors_len=np.expand_dims(priors_len, axis=0)
+    priors_len=np.delete(priors_len, indexes_not_linked,1) 
+    return priors_len /np.sum(priors_len)
+    #return priors_len/np.sum(priors_len)
+
+
+
+
+
+def PR_priorsPosition_weightsBM25(sentences):
+    sentences_vectors,counts_of_terms= get_score_BM5(sentences)
+    graph,indexes, indexes_sents_not_linked=get_graph(sentences_vectors)
+    priors=get_prior_Position(len(sentences_vectors),indexes_sents_not_linked)
+    PR=calculate_improved_prank(graph, 0.15,50,  priors, indexes)
+    print("\n SOMA", sum(list(PR.values())))
+    summary, summary_to_user=exercise_1.show_summary(PR,sentences,5)
+    return summary, summary_to_user
+
+def get_score_BM5(content):
+    k=1.2
+    b=0.75
+    
+    vec = CountVectorizer()
+    counts_of_terms=vec.fit_transform(content).toarray()
+    
+    nominator=counts_of_terms*(k+1)
+    length_of_sentences_D=counts_of_terms.sum(1)
+    number_sentences=len(counts_of_terms)
+    avgdl= sum(length_of_sentences_D)/number_sentences
+    denominator=counts_of_terms+(k*(1-b+b*((length_of_sentences_D)/(avgdl))))[:, None] 
+    score_BM5_without_ISF=nominator/denominator
+    
+    N=len(counts_of_terms) #numero de frases
+    nt=(counts_of_terms!=0).sum(0)  #nº vezes q o termo aparece nas frases
+    isfs=np.log10((N-nt+0.5)/(nt+0.5))  # inverve sentence frequency   
+         
+    return score_BM5_without_ISF*isfs, counts_of_terms
+
 
 
 def get_graph(sentences_vectors):
@@ -136,12 +222,7 @@ def get_graph(sentences_vectors):
     #print("graph after", len(graph))
     #print("Graph\n", graph)
 
-
-    
     return graph,indexes_sents, indexes_sents_not_linked
-
-
-
 
 
     
@@ -159,13 +240,14 @@ def calculate_improved_prank(graph, damping, n_iter, priors, indexes):
 
 
 def calculate_precision_recall_ap(summary, ideal_summary_allContent,ideal_summary_sentences,
-            AP_sum ):
+            AP_sum, precision_sum ):
     
     R=len(ideal_summary_sentences)  #documento relevante
     
     RuA = sum(1 for x in summary if x in ideal_summary_allContent)
     precision= RuA / len(summary)
     print("Precision", precision)
+    precision_sum+=precision
 
     correct_until_now = 0
     precisions = 0
@@ -175,7 +257,7 @@ def calculate_precision_recall_ap(summary, ideal_summary_allContent,ideal_summar
             precisions+= correct_until_now / (i+1)
                     
     AP_sum+=(precisions/R)
-    return AP_sum 
+    return AP_sum, precision_sum
 
 
 def sentences_ToVectorSpace(content):
@@ -197,10 +279,19 @@ def doc_ToVectorSpace(isfs, counts_of_terms_sent):
     tfs_doc=counts_of_terms/np.max(counts_of_terms, axis=1)[:, None]
     return tfs_doc*isfs
 
+def cosine_similarity(main_sentence,sentences_vectors ):
+    cosSim=[]
+    for sentence_vector in sentences_vectors:
+        cosSim.append( np.dot(sentence_vector,main_sentence)/(np.sqrt(
+            np.sum(sentence_vector*sentence_vector) )* np.sqrt(np.sum(main_sentence*main_sentence) )))
+    return np.array(cosSim) 
+
+
+
 
 
 def readTest():
-    file_content,sentences=exercise_1.getFile_and_separete_into_sentences("script1.txt")
+    file_content,sentences=getFile_and_separete_into_sentences("script1.txt")
     
     ex1_sents_represented=exercise_1.sentences_ToVectorSpace(sentences)  
     ex1_graph=exercise_1.get_graph(ex1_sents_represented, 0.2)     
@@ -223,6 +314,10 @@ if __name__ == "__main__":
     ideal_summaries_filesPath=get_ideal_summaries_files('TeMario/Sumarios/Extratos ideais automaticos')
     n_docs=read_docs('TeMario/Textos-fonte/Textos-fonte com titulo')       
     print("\n exercise 1- MAP", (ex1_AP_sum / n_docs))
+    print("\n exercise 1- Precision", (ex1_precision_sum / n_docs))
+
     print("\n exercise 2- MAP", (ex2_AP_sum / n_docs))
+    print("\n exercise 2- Precision", (ex2_precision_sum / n_docs))
+
     
     #readTest()
